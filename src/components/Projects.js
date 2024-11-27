559f50ad443b9280f8de72e08fdc28afdb35ac94
pemani98 <priyaemani1@gmail.com>
2024-11-27 08:48:36 -0500

    setup gh-pages

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,14 +2,13 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="projects">
-      <h2>My Projects</h2>
-      <ul>
-        <li>
-          
-        </li>
-
-      </ul>
+    <div className="projectsContainer">
+       <h1 style={{ color: "#165fc3" , fontFamily: 'Londrina Sketch', fontSize:'56px', textAlign:'center' }}>My Projects</h1>
+        <div className = "projectList">
+          <div className ="myProject" > Coffee </div>
+          <div className ="myProject" > Art Project </div>
+        </div>
+      
     </div>
   );
 };
